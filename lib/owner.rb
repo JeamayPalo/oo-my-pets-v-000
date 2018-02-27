2f2d8bb39236cd2e96fb787d7047f336ee12c73c
Jeamay Palo <jeamay.palo@gmail.com>
2018-02-27 00:48:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,38 +1,42 @@
 class Owner
-  
+
   @@all = []
-  
-  attr_accessor :name, :pets 
+
+  attr_accessor :name, :pets
   attr_reader :species
-  
+
   def initialize(species)
     @species = species
-    @@all << self 
+    @@all << self
     @pets = {:fishes => [], :cats => [], :dogs => []}
   end
-  
-  #Class Methods 
-  
+
+  #Class Methods
+
   def self.all
     @@all
-  end 
-  
-  def self.reset_all 
-    @@all.clear  
-  end 
-  
-  def self.count 
-    @@all.size 
-  end 
-  
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
   #Instance Methods
 
   def say_species
     "I am a #{@species}."
   end
-  
-   def buy_fish(name_of_fish)
-    @pets[:fishes] << Fish.new(name_of_fish)
-  `end
-    
+
+  def buy_fish(fish)
+    @pets[:fishes] << Fish.new(fish)
+  end
+
+  def buy_dog(name_of_fish)
+    @pets[:fishes] << Fish.new(fish)
+  end
+
 end
